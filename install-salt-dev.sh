5b52a9e4f3092de6cc0c534505605b9e2dc4bb23
airmetal <andrew.das@itaas.dimensiondata.com>
2016-12-16 13:29:27 -0500

    Added new sample profiles and maps

--- install-salt-dev.sh
+++ install-salt-dev.sh
@@ -2,6 +2,8 @@
 yum -y update
 yum -y install git-all
 yum -y install gcc
+yum -y install openssl-devel
+yum -y install python-devel
 yum -y install wget
 wget https://pypi.python.org/packages/source/s/setuptools/setuptools-7.0.tar.gz --no-check-certificate
 tar xzf setuptools-7.0.tar.gz
@@ -9,12 +11,25 @@ cd setuptools-7.0
 python setup.py install
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
-virtualenv ~/.salt
+pip install virtualenv
+virtualenv --system-site-packages ~/.salt
 source ~/.salt/bin/activate
 pip install M2Crypto
 pip install pyzmq PyYAML pycrypto msgpack-python jinja2 psutil futures tornado
+pip install apache-libcloud
+pip install netaddr
 cd /opt
 git clone https://github.com/DimensionDataSA/salt.git
 pip install -e ./salt
 mkdir -p ~/.salt/etc/salt
-cp ./salt/conf/master ./salt/conf/minion ~/.salt/etc/salt
+cd
+rm -rf setuptools*
+cp -r /opt/salt/conf/* ~/.salt/etc/salt/
+cd ~/.salt/etc/salt/cloud.providers.d/
+wget http://s3.amazonaws.com/ddsalt/etc/salt/cloud.providers.d/dimensiondata.conf
+cd ~/.salt/etc/salt/cloud.profiles.d/
+wget  http://s3.amazonaws.com/ddsalt/etc/salt/cloud.profiles.d/didata-web.conf 
+cd ~/.salt/etc/salt/cloud.maps.d/ 
+wget  http://s3.amazonaws.com/ddsalt/etc/salt/cloud.maps.d/didata-web-centos.conf 
+
+
